2ca8b8661654c726a5aa5696b4882f55eb3c2460
hagger <haggersoma@gmail.com>
2023-08-23 16:40:11 +0300

    create edit note view

--- lib/widgets/custom_noteitem.dart
+++ lib/widgets/custom_noteitem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/widgets/editviewbody.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem();
@@ -46,45 +47,52 @@ class NoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     i++;
     return i < colors.length
-        ? Container(
-            padding: const EdgeInsets.only(top: 24, bottom: 24),
-            decoration: BoxDecoration(
-              color: colors[i],
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                ListTile(
-                  title: Text(
-                    "Flutter tips",
-                    style: TextStyle(color: Colors.black, fontSize: 26),
+        ? GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+return const EditViewBody();
+            }));
+          },
+          child: Container(
+              padding: const EdgeInsets.only(top: 24, bottom: 24),
+              decoration: BoxDecoration(
+                color: colors[i],
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  ListTile(
+                    title: Text(
+                      "Flutter tips",
+                      style: TextStyle(color: Colors.black, fontSize: 26),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 16, bottom: 16),
+                      child: Text(
+                        "build your career with hagger esmail",
+                        style: TextStyle(color: Colors.black38, fontSize: 20),
+                      ),
+                    ),
+                    trailing: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          FontAwesomeIcons.trash,
+                          color: Colors.black,
+                          size: 24,
+                        )),
                   ),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.only(top: 16, bottom: 16),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
                     child: Text(
-                      "build your career with hagger esmail",
-                      style: TextStyle(color: Colors.black38, fontSize: 20),
+                      "4 june ,2001",
+                      style: TextStyle(color: Colors.black38, fontSize: 16),
                     ),
                   ),
-                  trailing: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        FontAwesomeIcons.trash,
-                        color: Colors.black,
-                        size: 24,
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 16),
-                  child: Text(
-                    "4 june ,2001",
-                    style: TextStyle(color: Colors.black38, fontSize: 16),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
+        )
         : Container(
             padding: const EdgeInsets.only(top: 24, bottom: 24),
             decoration: BoxDecoration(
