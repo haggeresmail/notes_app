d477ff3666f90eb130bff008c25f91a2fe51a360
hagger <haggersoma@gmail.com>
2023-08-24 12:53:56 +0300

    add add note cubit to add note bottom sheet

--- lib/cubits/cubit/add_note_cubit.dart
+++ lib/cubits/cubit/add_note_cubit.dart
@@ -8,11 +8,11 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  addNote(NoteModel note) {
+  addNote(NoteModel note)async {
     emit(AddNoteLoading());
     try {
       var notesbox = Hive.box<NoteModel>(kNotesBox);
-      notesbox.add(note);
+     await notesbox.add(note);
       emit(AddNoteSuccess());
     } catch (e) {
       emit(AddNoteFailure(errMessage: e.toString()));
---
+++ lib/widgets/addnote_form.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp/widgets/custom_buttonsheet.dart';
+import 'package:noteapp/widgets/custom_textfield.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      autovalidateMode: autovalidateMode,
+      key: formKey,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 24,
+          ),
+          CustomTextField(
+            onSaved: (data) {
+              title = data;
+            },
+            hintText: "Title",
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          CustomTextField(
+            hintText: "content",
+            onSaved: (data) {
+              subtitle = data;
+            },
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          CustomButton(
+            text: 'Add',
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
